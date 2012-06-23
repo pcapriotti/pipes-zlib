41b12351fe33e47f8226a1f650c211604bc5b110
Paolo Capriotti <p.capriotti@gmail.com>
2012-06-23 23:25:28 +0100

    Follow changes in pipes-core 0.2.0.

--- Control/Pipe/Zlib.hs
+++ Control/Pipe/Zlib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Control.Pipe.Zlib (
   gzip,
   gunzip,
@@ -9,49 +10,45 @@ import Codec.Zlib
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Pipe
-import Control.Pipe.Combinators
+import Control.Pipe.Class
 import qualified Data.ByteString as B
 import Prelude hiding (catch)
 
 -- | Gzip compression with default parameters.
-gzip :: MonadIO m => Pipe B.ByteString B.ByteString m r
+gzip :: (MonadStream m, MonadIO (BaseMonad m)) => m B.ByteString B.ByteString r r
 gzip = compress 1 (WindowBits 31)
 
 -- | Gzip decompression with default parameters.
-gunzip :: MonadIO m => Pipe B.ByteString B.ByteString m r
+gunzip :: (MonadStream m, MonadIO (BaseMonad m)) => m B.ByteString B.ByteString r r
 gunzip = decompress (WindowBits 31)
 
 decompress
-    :: MonadIO m
+    :: (MonadStream m, MonadIO (BaseMonad m))
     => WindowBits
-    -> Pipe B.ByteString B.ByteString m r
+    -> m B.ByteString B.ByteString r r
 decompress config = do
     inf <- liftIO $ initInflate config
-    let finalize = do chunk <- liftIO $ finishInflate inf
-                      unless (B.null chunk) $ yield chunk
-    forP' finalize $ \x -> do
+    r <- forP $ \x -> do
       popper <- liftIO $ feedInflate inf x
       yieldPopper popper
+    do chunk <- liftIO $ finishInflate inf
+       unless (B.null chunk) $ yield chunk
+    return r
 
 compress
-    :: MonadIO m
+    :: (MonadStream m, MonadIO (BaseMonad m))
     => Int
     -> WindowBits
-    -> Pipe B.ByteString B.ByteString m r
+    -> m B.ByteString B.ByteString r r
 compress level config = do
     def <- liftIO $ initDeflate level config
-    let finalize = yieldPopper (finishDeflate def)
-    forP' finalize $ \x -> do
+    r <- forP $ \x -> do
       popper <- liftIO $ feedDeflate def x
       yieldPopper popper
+    yieldPopper (finishDeflate def)
+    return r
 
-forP' :: Monad m
-      => Pipe a b m r2
-      -> (a -> Pipe a b m r1)
-      -> Pipe a b m r
-forP' p f = forP f >> p >> discard
-
-yieldPopper :: MonadIO m => Popper -> Pipe a B.ByteString m ()
+yieldPopper :: (MonadStream m, MonadIO (BaseMonad m)) => Popper -> m a B.ByteString u ()
 yieldPopper pop = do
   x <- liftIO pop
   case x of
